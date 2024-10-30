fd10358387021560ccdf0d62e6fde997bdd317e4
CYP3RBOT <cyp3rbot8009@gmail.com>
2024-10-30 18:53:39 -0400

    First commit

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,31 @@
+local plugins = {
+  {
+    "github/copilot.vim",
+    lazy = false
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require("custom.configs.null-ls")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format"
+      }
+    }
+  }
+}
+
+return plugins
